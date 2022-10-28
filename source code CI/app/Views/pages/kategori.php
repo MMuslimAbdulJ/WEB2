d1938bbcc3a426d26aa24c0e1453c9c717198b3e
MMuslimAbdulJ <mmuslimabdulj@gmail.com>
2022-10-28 01:33:35 -0700

    change something

--- source code CI/app/Views/pages/kategori.php
+++
@@ -1,8 +0,0 @@
-<?= $this->extend('layout/template'); ?>
-
-<?= $this->section('isi'); ?>
-<h1>Katalog</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti dolore consequatur unde, molestias hic eveniet
-    mollitia dolorem rem, quidem assumenda delectus earum officiis necessitatibus voluptas vitae numquam. Est, cum
-    possimus.</p>
-<?= $this->endSection(); ?>
